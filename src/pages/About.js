6de68574dbff25c8d140b20f2e30e0ab1050311e
Bethpen <jesutofunmiosunlana@gmail.com>
2022-07-19 14:15:42 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae saepe, tempora voluptatum in doloremque velit, soluta placeat, incidunt perferendis praesentium nostrum minima exercitationem cumque veritatis qui ea reiciendis deserunt labore?</p>
+    </section>
+  )
+}
+
+export default About
